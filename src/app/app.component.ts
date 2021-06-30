22974676e63f33436cb191597cd75d84211eb203
alekay <alekaymusic@gmail.com>
2021-06-30 15:20:28 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { OktaAuthService } from '@okta/okta-angular';
+
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  isAuthenticated?: boolean;
+
+  constructor(public oktaAuth: OktaAuthService, public router: Router) {
+    // Subscribe to authentication state changes
+    this.oktaAuth.$authenticationState.subscribe(
+      (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
+    );
+  }
+
+  async ngOnInit() {
+    // Get the authentication state for immediate use
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
+
+  login() {
+    this.oktaAuth.signInWithRedirect({
+      originalUri: '/profile'
+    });
+  }
+
+  async logout() {
+    // Terminates the session with Okta and removes current tokens.
+    await this.oktaAuth.signOut();
+    this.router.navigateByUrl('/');
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule, Router } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ProtectedComponent } from './protected.component';
+import { LoginComponent } from './login.component';
+
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
@@ -12,11 +15,62 @@ import { CartComponent } from './cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ShippingComponent } from './shipping/shipping.component';
 
+import {
+  OKTA_CONFIG,
+  OktaAuthModule,
+  OktaCallbackComponent,
+  OktaAuthGuard
+} from '@okta/okta-angular';
 
+const config = {
+  issuer: 'https://dev-62376248.okta.com/oauth2/default',
+  redirectUri: window.location.origin + '/login/callback',
+  clientId: '0oa12pg1938paTMyC5d7',
+  pkce: true
+}
+
+export function onAuthRequired(oktaAuth: any, injector: { get: (arg0: typeof Router) => any; }) {
+  const router = injector.get(Router);
+
+  // Redirect the user to your custom login page
+  router.navigate(['/login']);
+}
+
+const appRoutes: Routes = [
+  {
+    path: 'login/callback',
+    component: OktaCallbackComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ OktaAuthGuard ],
+    data: {
+      onAuthRequired
+    }
+  }
+]
 @NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProtectedComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
+  ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     HttpClientModule,
+    OktaAuthModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
@@ -25,14 +79,8 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: 'shipping', component: ShippingComponent },
     ])
   ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent,
-    ProductAlertsComponent,
-    ProductDetailsComponent,
-    CartComponent,
-    ShippingComponent
+  providers: [
+    { provide: OKTA_CONFIG, useValue: config }
   ],
   bootstrap: [AppComponent]
 })
